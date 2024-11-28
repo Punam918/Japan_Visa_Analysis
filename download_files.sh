7be1ee4910f95bbaf618f4338d2341a17f51b92e
punamcancodee <077bct062.punam@pcampus.edu.np>
2024-11-28 13:57:40 +0545

    Initialcommit

---
+++ download_files.sh
@@ -0,0 +1,2 @@
+scp -i spark-clusters_key.pem -rp azureuser@20.117.105.62:/home/azureuser/output/* src/output
+
---
+++ upload_files.sh
@@ -0,0 +1,2 @@
+scp -i spark-clusters_key.pem -r ./src/* azureuser@20.117.105.62:/home/azureuser
+
